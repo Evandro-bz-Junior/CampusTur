c2607bde1729ce862a4734960dd37d275796fad9
Evandro-bz-junior <evandrobzjunior@gmail.com>
2024-07-17 16:43:01 -0300

    alteração no css para tablets, transparencia da navbar e no scrollreveal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,6 +11,7 @@ window.onscroll = function () {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+//
 
 var hamburger = document.querySelector(".hamburger");
 var mobileNavLinks = document.querySelector(".nav-links");
@@ -19,6 +20,13 @@ var navLinks = document.querySelectorAll(".nav-links a");
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     mobileNavLinks.classList.toggle("active");
+
+    // Adiciona ou remove a classe no-scroll no body
+    if (mobileNavLinks.classList.contains('active')) {
+        document.body.classList.add('no-scroll');
+    } else {
+        document.body.classList.remove('no-scroll');
+    }
 });
 
 // Adiciona evento de clique a todos os links do menu
@@ -27,14 +35,19 @@ navLinks.forEach(function(link) {
         // Fecha o menu ao clicar no link
         hamburger.classList.remove("is-active");
         mobileNavLinks.classList.remove("active");
+        
+        // Remove a classe no-scroll no body
+        document.body.classList.remove('no-scroll');
     });
 });
 
+
+//
 const sr = ScrollReveal({
-    origin: 'top' ,
-    distance: '60px',
-    duration: 3000,
-    delay: 400,
+    origin: 'top',
+    distance: '30px', // Reduz a distância
+    duration: 2000, // Reduz a duração
+    delay: 300, // Reduz o atraso
     //reset: true //animation repeat
 })
  
@@ -43,6 +56,6 @@ sr.reveal(`.nav-item, .contact-header`)
 sr.reveal(`.navbar-brand, .home-section .left-side, .About-city .left-side, .about-us .container1 .left-side, .about-us .container2 .right-side, .about-us .container3 .left-side, .attraction1 .attraction-info, .attraction3 .attraction-info, #carousel2, .contact-content .img-area`,{origin:'left'})
 sr.reveal(`.home-section .right-side, .About-city .right-side, .about-us .container1 .right-side, .about-us .container2 .left-side, .about-us .container3 .right-side, .attraction2 .attraction-info, #carousel1, #carousel3, .contact-content .contact-area`,{origin:'right'})
 sr.reveal(`.about-city-img2, .destiny-header`,{origin:'bottom'})
-sr.reveal(`.about-city-img1, .gallery-item, .testimony-1, .testimony-3`,{origin:'left',interval: 300})
-sr.reveal(`.about-city-img3, .other-attractions .item, .testimony-2`,{origin:'right',interval: 300})
+sr.reveal(`.about-city-img1, .gallery-item, .testimony-1, .testimony-3`,{origin:'left',interval: 200})
+sr.reveal(`.about-city-img3, .other-attractions .item, .testimony-2`,{origin:'right',interval: 200})
 sr.reveal(`.nav-item,.corp-footer, .links, .infos-footer`,{interval: 200})
